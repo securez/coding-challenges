3e67ad1374ec74f923ffcd009419b141f2a474fe
Marcos Lois Bermúdez <marcos.lois@gmail.com>
2020-01-19 22:06:31 +0100

    feat: Add simple golang ascii stripe draw.
    Can be easy transformed in stream version using io.Writer for huge size, logos, ;D.

--- adidas-ascii-logo/go/main.go
+++ adidas-ascii-logo/go/main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"math"
+	"strings"
 )
+
 func createAdidasAsciiLogo(width int) string {
-  return fmt.Sprintf("%d",width)
+	if width < 2 {
+		panic("Error, minimum width is 2")
+	}
+
+	var sb strings.Builder
+	numStripes := 3 // Can be any number of numStripes
+	smallHeight := int(math.Round(math.Sqrt(float64(width))))
+	totalHeight := smallHeight * numStripes
+
+	// Some vars to speed up things a little bit
+	stripe := strings.Repeat("@", width)
+	separator := strings.Repeat(" ", smallHeight)
+	stripes := make([]string, numStripes)
+	for i := range stripes {
+		stripes[i] = stripe
+	}
+
+	for h := 0; h < totalHeight; h++ {
+		offset := h % smallHeight
+		skip := numStripes - 1 - (h / smallHeight)
+
+		sb.WriteString(strings.Repeat(" ", (skip*width)+offset))
+		sb.WriteString(strings.Join(stripes[skip:], separator))
+
+		// Avoid last carriage return
+		if h != totalHeight-1 {
+			sb.WriteString("\n")
+		}
+	}
+
+	return sb.String()
 }
 
-func main(){
+func main() {
 
-  widths := []int{2, 3, 5, 7, 9, 16, 21}
+	widths := []int{2, 3, 5, 7, 9, 16, 21}
 
-  for _, width := range widths {
-    fmt.Println(fmt.Sprintf("\nadidas (width %d)",width))
-    fmt.Println("\n-------------------------------------------------------------")
-    fmt.Println(fmt.Sprintf("\n%s\n\n",createAdidasAsciiLogo(width)))
-  }
+	for _, width := range widths {
+		fmt.Println(fmt.Sprintf("\nadidas (width %d)", width))
+		fmt.Println("\n-------------------------------------------------------------")
+		fmt.Println(fmt.Sprintf("\n%s\n\n", createAdidasAsciiLogo(width)))
+	}
 }
