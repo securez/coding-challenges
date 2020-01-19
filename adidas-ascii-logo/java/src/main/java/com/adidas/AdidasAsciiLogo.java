237d32d883b01a4b80cd5288e27e53a3e2cd695f
Marcos Lois Bermúdez <marcos.lois@gmail.com>
2020-01-19 22:07:44 +0100

    feat: Add simple java ascii stripe draw.
    Can be easy transformed in stream version using java.io.Writer for huge size, logos, ;D.

--- adidas-ascii-logo/java/src/main/java/com/adidas/AdidasAsciiLogo.java
+++ adidas-ascii-logo/java/src/main/java/com/adidas/AdidasAsciiLogo.java
@@ -1,9 +1,44 @@
 package com.adidas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AdidasAsciiLogo {
 
-    public String createAdidasAsciiLogo(int width) {
-        return null;
+    private String repeat(String str, int count) {
+        return count > 0 ?  repeat(str, count - 1) + str: "";
+    }
 
+    public String createAdidasAsciiLogo(int width) {
+        if (width < 2) {
+            throw new IllegalArgumentException("minimum width is 2");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int numStripes = 3;
+        int smallHeight = (int) Math.round(Math.sqrt(width));
+        int totalHeight = smallHeight * numStripes;
+
+        // Some vars to speed up things a little bit
+        String stripe = this.repeat("@", width);
+        String separator = this.repeat(" ", smallHeight);
+        List<String> stripes = new ArrayList<String>();
+        for (int i = 0; i < numStripes; i++) {
+            stripes.add(stripe);
+        }
+
+        for (int h = 0; h < totalHeight; h++) {
+            int offset = h % smallHeight;
+            int skip = numStripes - 1 - (h / smallHeight);
+
+            sb.append(this.repeat(" ", skip * width + offset));
+            sb.append(String.join(separator, stripes.subList(skip, numStripes)));
+
+            // Java Test expects carriage return on all lines
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 }
