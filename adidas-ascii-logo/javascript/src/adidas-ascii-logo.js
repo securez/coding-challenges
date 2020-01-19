f7ab747f6b4899372843a21231ea06746d223cff
Marcos Lois Bermúdez <marcos.lois@gmail.com>
2020-01-19 22:10:57 +0100

    feat: Add simple javascript ascii stripe draw.
    Can be easy transformed in stream, javascript is experimenting with WritableStream but not standard.

--- adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -4,5 +4,32 @@
  * @returns {string} adidas logo.
  */
 module.exports = function(width) {
-  return `${ width }`;
+  if (width < 2) {
+    throw 'Error, minimum width is 2';
+  }
+
+  let str = '';
+  let numStripes = 3;
+  let smallHeight = Math.round(Math.sqrt(width));
+  let totalHeight = smallHeight * numStripes
+
+  // Some vars to speed up things a little bit
+  let stripe = '@'.repeat(width);
+  let separator = ' '.repeat(smallHeight);
+  let stripes = Array(numStripes).fill(stripe);
+
+  for (let h = 0; h < totalHeight; h++) {
+    let offset = h % smallHeight;
+    let skip = numStripes - 1 - Math.floor(h / smallHeight);
+
+    str += ' '.repeat( (skip * width) + offset);
+    str += stripes.slice(skip, numStripes).join(separator);
+
+    // Tests expect no last carriage return
+    if (h != totalHeight - 1) {
+      str += '\n';
+    }
+  }
+
+  return str;
 };
